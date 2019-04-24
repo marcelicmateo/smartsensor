739b5139c49f3cdf00acf456ec0fd680bd53f2b1
Mateo Marcelic <kloki2004@gmial.com>
2019-04-24 15:21:17 +0200

    obrada signala dovrsena, javlja temperaturu

--- adc_source/obrada.py
+++ adc_source/obrada.py
@@ -1,7 +1,8 @@
 import datetime 
 import numpy
-from matplotlib import pyplot
+#from matplotlib import pyplot
 import json
+import math
 
 st = datetime.datetime.utcnow().strftime('%Y-%m-%d_%H:%M:%S:%f')
 num_of_char_to_read=8*100+100
@@ -38,6 +39,25 @@ def obrada_kanala(fname):
 kanal1=obrada_kanala('kanal1')
 kanal2=obrada_kanala('kanal2')
 
+rs=10000
+srs=rs*0.01         #devijacija otpora shunta
+surs=kanal2[2]      #devijacija napona shunta
+suntc=kanal1[2]     #devijacija napona ntca
+
+rntc=kanal1[1]/kanal2[1] * rs  #otpor ntca
+
+
+Untc=rs*suntc/kanal1[1]
+Ushunt=kanal1[1]*rs*surs/kanal2[1]**2
+Urshunt=kanal1[1]*srs/kanal2[1]
+
+s=math.sqrt(Untc**2+Urshunt**2+Ushunt**2)       #devijacija otpora ntca
+
+temperatura=(math.log(rntc/22000)/3740 + 1/(25+273.15))**(-1)
+
+print(temperatura - 273.15) 
+
+
 
 f=open("data_log.txt","ab")
 log={"timestamp": st,
@@ -47,8 +67,8 @@ log={"timestamp": st,
      "kanal2":kanal2[0],
      "kanal2_mean":kanal2[1],
      "kanal2_std":kanal2[2],
-     }
 
+     }
 
 
 
