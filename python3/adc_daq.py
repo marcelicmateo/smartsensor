64d6cad789fc10aebb2f1293e0267c02c17a775b
Marcelic mateo <marcelicmateo@gmail.com>
2020-01-22 14:27:15 +0000

    napisana funkkcija za skupljanje podataka sa adc-a

---
+++ python3/adc_daq.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+import time
+import ADS1256
+import RPi.GPIO as GPIO
+from numpy import zeros, int32
+
+def adc_daq(number_of_samples, sps):
+    ADC = ADS1256.ADS1256()
+    ADC.ADS1256_init()
+
+    channels=zeros([2,number_of_samples], dtype=int32)
+
+    for i in range(number_of_samples):
+        channels[0][i]=ADC.ADS1256_GetChannalValue(0)
+        channels[1][i]=ADC.ADS1256_GetChannalValue(1)
+    
+    GPIO.cleanup()
+    print("DAQ done\n")
+    return channels
+
+if __name__ == "__main__":
+    c=adc_daq(100,'ADS1256_3750SPS')
+    print(c)
+    pass
+    
